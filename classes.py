78c897c0542d836bfd5b8e01584eebc29d90f519
Ivan Parkhomenko <ippeng54@gmail.com>
2023-05-10 10:18:17 +0300

    add functions2

--- classes.py
+++ classes.py
@@ -1,6 +1,50 @@
 from abc import ABC, abstractmethod
-
+import json
+import os
 import requests
+from pprint import pprint
+
+
+class ParsingError(Exception):
+    def __str__(self):
+        return 'Ошибка получения данных по API.'
+
+
+class Vacancy:
+    __slots__ = ('id', 'title', 'url', 'salary_from', 'salary_to', 'employer', 'api')
+
+    def __init__(self, vacancy_id, title, url, salary_from, salary_to, employer, api):
+        self.id = vacancy_id
+        self.title = title
+        self.url = url
+        self.salary_from = salary_from
+        self.salary_to = salary_to
+        self.employer = employer
+        self.api = api
+
+    def __gt__(self, other):
+        if not other.salary_min:
+            return True
+        elif not self.salary_min:
+            return False
+
+
+class Connector:
+    def __int__(self, keyword, vacancies_json):
+        self.__filename = f"{keyword.title()}.json"
+        self.insert(vacancies_json)
+
+    def insert(self, vacancies_json):
+        with open(self.__filename, 'w', encoding='utf-8') as file:
+            json.dump(vacancies_json, file, ensure_ascii=False, indent=4)
+
+    def select(self):
+        # vacancies = []
+        with open(self.__filename, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+        vacancies = [Vacancy(x['id'], x['title'], x['salary_from'], x['salary_to'], x['employer'], x['api']) for x in
+                     data]
+        return vacancies
 
 
 class Engine(ABC):
@@ -15,11 +59,12 @@ class Engine(ABC):
 
 class HeadHunter(Engine):
     def __init__(self, keyword):
-        self.__header = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:50.0) Gecko/20100101 Firefox/50.0"}
+        self.__header = {
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:50.0) Gecko/20100101 Firefox/50.0"}
         self.__params = {
             "text": keyword,
-            "page": 0,  #номер страницы
-            "per_page": 100  #количество отображаемых записей на странице
+            "page": 0,  # номер страницы
+            "per_page": 100  # количество отображаемых записей на странице
         }
         self.__vacancies = []
 
@@ -66,6 +111,52 @@ class HeadHunter(Engine):
         return formatted_vacancies
 
 
+class SuperJob(Engine):
+    def __init__(self, keyword):
+        self.__header = {"X-Api-App-Id": os.getenv("SJ_API_KEY")}
+        self.__params = {
+            "text": keyword,
+            "page": 0,  # номер страницы
+            "count": 100  # количество отображаемых записей на странице
+        }
+        self.__vacancies = []
 
-class SuperJob (Engine):
-    pass
+    @staticmethod
+    def get_salary(salary, currency):
+        formatted_salary = None
+        if salary and salary != 0:
+            formatted_salary = salary if currency == 'rub' else salary * 80
+        return formatted_salary
+
+    def get_request(self):
+        response = requests.get('https://api.superjob.ru/2.0/vacancies', headers=self.__header, params=self.__params)
+        if response.status_code != 200:
+            raise ParsingError
+        return response.json()['objects']
+
+    def get_vacancies(self, pages_count=1):
+        while self.__params['page'] < pages_count:
+            print(f"SuperJob, Парсинг страницы {self.__params['page'] + 1}", end=": ")
+            try:
+                values = self.get_request()
+            except ParsingError:
+                print('Ошибка получения данных!')
+                break
+            print(f"Найдено ({len(values)}) вакансий.")
+            self.__vacancies.extend(values)
+            self.__params['page'] += 1
+
+    def get_formatted_vacancies(self):
+        formatted_vacancies = []
+        for vacancy in self.__vacancies:
+            salary_from, salary_to = self.get_salary(vacancy['salary'])
+            formatted_vacancies.append({
+                'id': vacancy['id'],
+                'title': vacancy['profession'],
+                'url': vacancy['link'],
+                'salary_from': self.get_salary(vacancy['payment_from'], vacancy['currency']),
+                'salary_to': self.get_salary(vacancy['payment_to'], vacancy['currency']),
+                'employer': vacancy['firm_name'],
+                'api': 'Superjob'
+            })
+        return formatted_vacancies
--- main.py
+++ main.py
@@ -1,16 +1,17 @@
+from classes import HeadHunter, SuperJob, Connector
 
 
 def main():
     vacancies_json = []
-    keyword = input('Введите ключевое слово для поиска: ')
-
-
+    #keyword = input('Введите ключевое слово для поиска: ')
+    keyword = 'Python'
     hh = HeadHunter(keyword)
     sj = SuperJob(keyword)
 
     for api in (hh, sj):
         api.get_vacancies(pages_count=1)
-        vacancies_json.extend(api_get_formatted_vacancies())
+        vacancies_json.extend(api.get_formatted_vacancies())
+        exit()
 
     connector = Connector(keyword=keyword, vacancies_json=vacancies_json)
 
@@ -21,13 +22,5 @@ def main():
         )
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
